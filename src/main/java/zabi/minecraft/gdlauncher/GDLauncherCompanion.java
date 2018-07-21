10e9ab263d2b000563cdb705d3393e13603282e8
zabi94 <zabi94@gmail.com>
2018-07-21 16:55:32 +0200

    Added networking

--- src/main/java/zabi/minecraft/gdlauncher/GDLauncherCompanion.java
+++ src/main/java/zabi/minecraft/gdlauncher/GDLauncherCompanion.java
@@ -1,5 +1,9 @@
 package zabi.minecraft.gdlauncher;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -25,9 +29,34 @@ public class GDLauncherCompanion {
 	public static final String MOD_VERSION = "0.0.1a";
 	public static final String MC_VERSION = "[1.8,1.14)";
 	
+	public static int port = 0;
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent evt) {
 		ModConfig.init(evt.getSuggestedConfigurationFile());
+		
+		String pathPortFile = evt.getSuggestedConfigurationFile().getParentFile().getParentFile().toString();
+		File portFile = new File(pathPortFile + File.separatorChar + "GDLPortComms.cfg");
+		
+		if (!portFile.exists() || portFile.isDirectory()) {
+			Log.e("Cannot find rendez-vous port. Use latest GDLauncher and GDLauncherCompanion mod");
+			return;
+		}
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(portFile));
+			port = Integer.parseInt(br.readLine());
+			br.close();
+		} catch (Exception e) {
+			Log.e("Invalid rendez-vous port configuration. Use latest GDLauncher and GDLauncherCompanion mod", e);
+			return;
+		}
+		
+		if (port<1000) {
+			Log.e("Invalid rendez-vous port found. Use latest GDLauncher and GDLauncherCompanion mod");
+			return;
+		}
+		Log.i("GDLauncherCompanion is using port "+port);
 		MinecraftForge.EVENT_BUS.register(this);
 		GameStatusListener.listen();
 	}
--- src/main/java/zabi/minecraft/gdlauncher/IPCDispatcher.java
+++ src/main/java/zabi/minecraft/gdlauncher/IPCDispatcher.java
@@ -1,5 +1,10 @@
 package zabi.minecraft.gdlauncher;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Objects;
 
 import com.google.gson.JsonObject;
@@ -7,14 +12,14 @@ import com.google.gson.JsonObject;
 public class IPCDispatcher {
 	
 	public static enum GameType {
-		MULTIPLAYER, LAN, SINGLEPLAYER, NONE
+		NONE, SINGLEPLAYER, MULTIPLAYER, LAN 
 	}
 	
 	
 	public static void setGamePlaying(GameType type, String name, String address, String motd, String iconBase64) {
 		Objects.requireNonNull(type);
 		JsonObject json = new JsonObject();
-		json.addProperty("type", type.name().toLowerCase());
+		json.addProperty("type", type.ordinal());
 		if (name!=null) {
 			json.addProperty("name", name);
 		}
@@ -27,6 +32,16 @@ public class IPCDispatcher {
 		if (iconBase64!=null) {
 			json.addProperty("icon", iconBase64);
 		}
-		Log.i(json.toString());
+		try {
+			Socket connection = new Socket("localhost", GDLauncherCompanion.port);
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+			bw.write(json.toString());
+			bw.close();
+			connection.close();
+		} catch (UnknownHostException e) {
+			Log.e("Cannot connect to GDLauncher service", e);
+		} catch (IOException e) {
+			Log.e("Cannot connect to GDLauncher service", e);
+		}
 	}
 }
